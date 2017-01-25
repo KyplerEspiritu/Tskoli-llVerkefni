897bc728bec96ee751b600a66b939d6d08c6db54
KyplerEspiritu <kypleres312@nemi.tskoli.is>
2017-01-25 10:56:28 +0000

    Daemi 1

--- git_forritun/gitforritunverk.py
+++ git_forritun/gitforritunverk.py
@@ -1,2 +1,17 @@
+#GitForritunVerkefni
+#Kypler Lloyd Espiritu
+#25.01.17
+
+#Dæmi
+
+tala1 = int(input("Sláðu inn tölu 1: "))
+tala2 = int(input("Sláðu inn tölu 2: "))
+
+marg = tala1 * tala2
+summ = tala1 + tala2
+
+print("Summa talnanna er", summ)
+print("Ef þú margfaldar tölurnar saman færðu",marg)
+
 
 
