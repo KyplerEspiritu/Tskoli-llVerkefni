796fb740b0eb1eb068d48efea46fc820f4ca757b
KyplerEspiritu <kypleres312@nemi.tskoli.is>
2017-01-25 12:10:58 +0000

    Daemi 3

--- git_forritun/gitforritunverk.py
+++ git_forritun/gitforritunverk.py
@@ -1,9 +1,9 @@
 #GitForritunVerkefni
 #Kypler Lloyd Espiritu
 #25.01.17
-
-#Dæmi
-
+''''
+#Dæmi 1
+print()
 tala1 = int(input("Sláðu inn tölu 1: "))
 tala2 = int(input("Sláðu inn tölu 2: "))
 
@@ -15,9 +15,35 @@ print("Ef þú margfaldar tölurnar saman færðu",marg)
 
 #Dæmi 2
 
-
+print()
 fornafn = input("Fornafn?: ")
 eftirnafn = input("Eftirnafn?: ")
 
 print("Halló",fornafn, eftirnafn)
+'''
+#Dæmi 3
+
+print()
+lagstaf = 0 #Lagstafir
+hastaf = 0 #Hastafir
+eftirha = 0 #Lagstafir eftir hastafir
+minus = -1
+texti = input("Sláðu inn texta: ")
+leng = int(len(texti))
+listi = list(texti)
+
+for x in listi:
+    if x.isupper():
+        hastaf += 1
+    elif x.islower():
+        lagstaf += 1
+
+for x in range(leng):
+    if listi[x].islower() and listi[minus].isupper():
+        minus += 1
+        eftirha += 1
+    else:
+        minus += 1
+
+print("Í þessum texta eru",hastaf,"hástafir,",lagstaf,"lágstafir og",eftirha,"lágstafir stra")
 
